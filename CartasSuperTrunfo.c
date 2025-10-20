f44d410d1ed6016a1a392e70e35e456781df68bf
danilo715 <danilo333889@gmail.com>
2025-10-20 23:27:29 +0000

    CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,13 +6,13 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-chat estado;
-chat codigo; [4]
-chat nome cidade[40]
+char estado;
+char codigo[4];
+char nomecidade[40];
 int populaçao;
 float area;
-float pib
-int pontos turistico;
+float pib;
+int pontosturistico;
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
